3a4e9822d8d6fc795c9ad70514ef399d4a6a1dc1
KanerA <kaner.assaf@gmail.com>
2021-01-18 14:38:06 +0200

    answer q5

---
+++ scripts/q5.js
@@ -0,0 +1,9 @@
+for(i=0; i<=15; i++){
+    if(i%2 === 0){
+        alert (i+ " is even");
+        // console.log("Number is even");
+    }
+    else    
+        alert (i+ " is odd");
+        // console.log("Number is odd");
+}
