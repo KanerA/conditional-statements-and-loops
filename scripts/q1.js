1a0548f6b6668f4154ffee035182e8e9887c162c
KanerA <kaner.assaf@gmail.com>
2021-01-18 13:18:50 +0200

    create+answer q1

---
+++ scripts/q1.js
@@ -0,0 +1,11 @@
+let num1= prompt("enter first number");
+let num2= prompt("enter second number");
+
+if(num1>num2){
+    alert(num1 + " is bigger");
+}
+
+else if(num2>num1){
+    alert(num2 + " is bigger");
+}
+else alert("the numbers are equal")
